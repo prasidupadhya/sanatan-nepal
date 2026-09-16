7dbefcb03951c580d3b3be717cffed9ea612f241
Prasid <prasidupadhya@gmail.com>
2026-09-16 10:19:04 +0200

    feat(scaffold): establish typed React SPA and validation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        maroon: '#421d27',
+        brass: '#c69b58',
+        terracotta: '#a54732',
+        parchment: '#f6efdf',
+      },
+    },
+  },
+  plugins: [],
+};
